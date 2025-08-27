32528861f54714176aaf5cbf306fde93d8a85691
Taha <tahaahmedmallick@gmail.com>
2025-08-27 22:31:03 +0500

    A small mistake.

--- chap-5/practice-set/problem7.c
+++ chap-5/practice-set/problem7.c
@@ -21,8 +21,8 @@ int main() {
 }
 
 void pattern(int lines) {
-    for (int i = 1; i <= lines; i++) {
-        for (int j = 1; j <= i; j++) printf("*");
+    for (int i = 0; i < lines; i++) {
+        for (int j = 0; j < 2*i+1; j++) printf("*");
         printf("\n");
     }
 }
